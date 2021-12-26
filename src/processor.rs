1dc804bf4eca901dc2b83c63f2cbacf6cfcb43f0
Alan_arakawa <alancodvo@gmail.com>
2021-12-27 02:52:56 +0900

    Add process init escrow function

--- src/processor.rs
+++ src/processor.rs
@@ -1,6 +1,7 @@
 use solana_program::{
-    account_info::AccountInfo,
+    account_info::{next_account_info, AccountInfo},
     entrypoint::ProgramResult,
+    program_error::ProgramError,
     msg,
     pubkey::Pubkey,
 };
@@ -18,5 +19,19 @@ impl Processor {
                 Self::process_init_escrow(accounts, amount, program_id)
             }
         }
+    }
+    fn process_init_escrow(
+        accounts: &[AccountInfo],
+        amount: u64,
+        program_id: &Pubkey,
+    ) -> ProgramResult {
+        let account_info_iter = &mut accounts.iter();
+        let initializer = next_account_info(account_info_iter)?;
+
+        if !initializer.is_signer {
+            return Err(ProgramError::MissingRequiredSignature);
+        }
+
+        Ok(())
     }
 }
